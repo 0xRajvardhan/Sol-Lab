d56ec54115a6e13754dcbdd6d21c689efd953489
CarlGustav <singhrajvardhan2003@gmail.com>
2025-03-16 14:05:33 +0530

    fetch sol balance

--- reading-on-sol/check-balance.ts
+++ reading-on-sol/check-balance.ts
@@ -1,10 +1,15 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-const publicKey = new PublicKey("uTo6DVXrSoopNvfBfofWrfc4umXyV6kKGTeWj71JFwd");
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+  throw new Error("Provide a public key to check the balance of!");
+}
+
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-const balanceInLamport = await connection.getBalance(publicKey);
-const balanceInSOL = balanceInLamport / LAMPORTS_PER_SOL;
+const publicKey = new PublicKey(suppliedPublicKey);
+const balanceInLamports = await connection.getBalance(publicKey);
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
 console.log(
-    `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`,
-  );
+  `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`,
+);
